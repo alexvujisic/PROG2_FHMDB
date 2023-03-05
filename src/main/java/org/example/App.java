1d60d4803bb19427545f9ba56717eed9796d5db7
alexvujisic <alexandar.vujisic@stud.fh-campuswien.ac.at>
2023-03-05 22:57:18 +0100

    1 commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+    public static int add(int i){
+        return (i + 1);
+    }
+}
